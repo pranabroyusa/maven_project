8f0264ca682df7528a94514a034b65facbe23e76
pranab roy <pranab roy>
2016-02-28 21:39:52 -0800

    1st

---
+++ roy.pranab/src/test/java/roy/pranab/Open_Browser.java
@@ -0,0 +1,12 @@
+package roy.pranab;
+
+import org.testng.annotations.Test;
+
+public class Open_Browser {
+	@Test
+	public void m1Test()
+	{
+		System.out.println("This is first maven Test");
+	}
+
+}
